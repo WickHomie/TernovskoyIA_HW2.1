526ddba7a32612e9a72fc791a4af20a5ab664666
wickapp <wickhomie@gmail.com>
2022-01-24 15:34:41 +0300

    Initial Commit

---
+++ Task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task1
+//
+//  Created by Илья Терновской on 24.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
